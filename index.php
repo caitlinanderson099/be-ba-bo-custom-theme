48103a8d79f6efe747158e11468a7560446241b1
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-12-05 10:14:36 +1300

    updated index.php with the deployed site link

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 <image src="<?php echo get_template_directory_uri(); ?>/images/shocked.png" alt="Shocked Face Image">
 <h1> Be Ba Bo Hair Studio Custom Theme </h1>
-<p> <a href="http://localhost:5173"> Click Here </a> To View <span> MyFirstWp.com </span> </p>
+<p> <a href="https://be-ba-bo-hair-studio.vercel.app/"> Click Here </a> To View <span> Be Ba Bo Hair Studio </span> </p>
